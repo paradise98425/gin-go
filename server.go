764fd5d4f19a73f23965e5116f1bffc4c7d762f7
Roshan Pratap Katel <paradise98425@gmail.com>
2021-04-27 16:53:45 +0545

    /posts endpoint added for FindAll

--- server.go
+++ server.go
@@ -1,14 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/paradise98425/go-gin/controller"
+	"github.com/paradise98425/go-gin/service"
+)
 
+var (
+	videoService service.VideoService = service.New()
+	VideoController controller.VideoController = controller.New(videoService)
+)
 
 func main() {
 	server := gin.Default()
-	server.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "Ok!",
-		})
+	server.GET("/posts", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.FindAll())
 	})
 
 	server.Run(":8081")
